49f0e0ee22f5e77abc43aaf22daa33494bc6db96
NyuonDeng <Deborahstrong17@gmail.com>
2022-11-10 20:01:52 -0500

    "json:target/cucumber.json" ,

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 
         plugin = {
+                "json:target/cucumber.json" ,
                 "pretty" ,
                 "html:target/cucumber-report.html" ,
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
